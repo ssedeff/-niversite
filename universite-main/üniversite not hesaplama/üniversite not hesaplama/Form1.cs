8e3c4affdfed996ce096fbde43f3b3f93ad71cd7
ssedeff <145463803+ssedeff@users.noreply.github.com>
2023-10-17 15:49:09 +0300

    Add files via upload

---
+++ universite-main/üniversite not hesaplama/üniversite not hesaplama/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace üniversite_not_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double vize ,final,ortalama;
+            vize = Convert.ToDouble(textBox1.Text);
+            final = Convert.ToDouble(textBox2.Text);
+            ortalama = (vize * 40 / 100)+(final *60/100);//ı >> i 
+            label3.Text = "ORTALAMA: " + ortalama;
+
+        }
+    }
+}
